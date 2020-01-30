abb9906dfa6d370431b0b992dc05f2871696c82b
Hamilton Turner <hamiltont@gmail.com>
2020-01-30 01:09:44 -0500

    Cleanup test suite and use mainTest by default
    Replaces the 'call an external binary' approach with the new
    'call a handler function in main.go' approach. Much faster.
    
    This commit also shows a few example test cases just to get
    the ball rolling

--- main_test.go
+++ main_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"sync"
 	"testing"
@@ -19,67 +18,83 @@ var (
 )
 
 const (
-	address = "localhost:9558"
+	address = "127.0.0.1:9558"
 )
 
-func TestWhatTheHack(t *testing.T) {
-	serverDone := &sync.WaitGroup{}
-	serverDone.Add(1)
-	// os.Args = []string{binaryName, "--version"}
-	os.Args = []string{binaryName, "--web.listen-address=127.0.0.1:9558", "--log.level=debug"}
-	srv := testMain(serverDone)
-
-	// Running some fancy tests right here :-)
-	time.Sleep(5 * time.Second)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		panic(err) // failure/timeout shutting down the server gracefully
-	}
-
-	serverDone.Wait()
+// TestNoop only exists as an example of how you can test
+func TestNoop(t *testing.T) {
+	noop := func() error { return nil }
+	runServerAndTest([]string{binaryName}, address, noop)
 }
 
-func testFlagVersion(t *testing.T) {
-	if _, err := os.Stat(binaryPath); err != nil {
-		t.Skipf("binary not available, try to run `go build` first: %s", err)
-	}
-	exporter := exec.Command(binaryPath, "--version", address)
-	if err := exporter.Run(); err != nil {
-		t.Error(err)
-	}
+// TestVersionFlag is an example of running a test that does not rely on the server being
+// online. TODO make a reusable runTest() for this use case
+func TestVersionFlag(t *testing.T) {
+	noop := func() error { return nil }
+	runServerAndTest([]string{binaryName, "--version"}, address, noop)
 }
 
-func testMetricsReturns200(t *testing.T) {
-	if _, err := os.Stat(binaryPath); err != nil {
-		t.Skipf("binary not available, try to run `go build` first: %s", err)
-	}
-
-	exporter := exec.Command(binaryPath, "--web.listen-address", address)
-	test := func(_ int) error {
-		resp, err := queryExporterMetrics()
+func TestMetricEndpointReturnsHttp200(t *testing.T) {
+	test := func() error {
+		resp, err := getMetrics()
 		if err != nil {
 			return err
 		}
 		if want, have := http.StatusOK, resp.StatusCode; want != have {
 			return fmt.Errorf("wanted status code %d, received %d", want, have)
 		}
-
 		return nil
 	}
+	runServerAndTest([]string{binaryName}, address, test)
+}
 
-	if err := runCommandAndTests(exporter, address, test); err != nil {
-		t.Error(err)
+func runServerAndTest(args []string, url string, fn func() error) error {
+	// Request server startup
+	serverDone := &sync.WaitGroup{}
+	serverDone.Add(1)
+	// TODO it would be cleaner to change main.go to use kingpin.MustParse
+	os.Args = args
+	srv := testMain(serverDone)
+
+	// ensure server is online before running test
+	fmt.Println("Waiting on test server startup...")
+	for i := 0; i < 10; i++ {
+		root := fmt.Sprintf("http://%s/", address)
+		if resp, err := getUrl(root); err == nil && resp.StatusCode == http.StatusOK {
+			break
+		}
+		time.Sleep(10 * time.Millisecond)
+		if i == 9 {
+			return fmt.Errorf("can't connect to %s - unable to run any tests", root)
+		}
 	}
+	fmt.Println("Test server ready, running test...")
+
+	// Run the test
+	err := fn()
+
+	// Shutdown the server before we return
+	fmt.Println("Test complete, shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel() // TODO is this correct?
+
+	if err := srv.Shutdown(ctx); err != nil {
+		// TODO is this what we shold do with serverDone?
+		defer serverDone.Wait()
+		return fmt.Errorf("failed to start command: %s", err)
+	}
+
+	serverDone.Wait()
+	fmt.Println("Test server shutdown, testcase complete.")
+
+	return err
 }
 
-func queryExporterMetrics() (*http.Response, error) {
-	return queryExporter(fmt.Sprintf("http://%s/metrics", address))
+func getMetrics() (*http.Response, error) {
+	return getUrl(fmt.Sprintf("http://%s/metrics", address))
 }
 
-func queryExporter(url string) (*http.Response, error) {
+func getUrl(url string) (*http.Response, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -95,34 +110,4 @@ func queryExporter(url string) (*http.Response, error) {
 	// 	return nil, fmt.Errorf("want /metrics status code %d, have %d. Body:\n%s", want, have, b)
 	// }
 	return resp, nil
-}
-
-func runCommandAndTests(cmd *exec.Cmd, address string, fn func(pid int) error) error {
-	if err := cmd.Start(); err != nil {
-		return fmt.Errorf("failed to start command: %s", err)
-	}
-
-	// ensure server is online before running test
-	time.Sleep(10 * time.Millisecond)
-	for i := 0; i < 10; i++ {
-		root := fmt.Sprintf("http://%s/", address)
-		if resp, err := queryExporter(root); err == nil && resp.StatusCode == http.StatusOK {
-			break
-		}
-		time.Sleep(500 * time.Millisecond)
-		if cmd.Process == nil || i == 9 {
-			return fmt.Errorf("can't connect to %s - unable to run any tests", root)
-		}
-	}
-
-	errc := make(chan error)
-	go func(pid int) {
-		errc <- fn(pid)
-	}(cmd.Process.Pid)
-
-	err := <-errc
-	if cmd.Process != nil {
-		cmd.Process.Kill()
-	}
-	return err
 }

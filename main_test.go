2351bdb232f638d1a6c1da6e3ba9f207475aa0d3
Hamilton Turner <hamiltont@gmail.com>
2020-01-29 22:40:36 -0500

    Initial test script based on node_exporter
    Main problem is that this does not support test coverage, because we are using an
    external binary as the binary-under-test and coverage works by compiling a custom
    binary with coverage-tracking-logic when running go test. It's not simple to force
    compilation of these tracking statements, so as long as we use an external binary we
    cannot have coverage data.
    
    Minor issue is that the process launch time is relatively slow. Not an issue when
    running 2-3 tests, but this is may be an issue down the road

---
+++ main_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sync"
+	"testing"
+	"time"
+)
+
+var (
+	binaryName = "systemd_exporter"
+	binaryPath = filepath.Join(binaryName)
+	// os.Getenv("GOPATH"), "bin/node_exporter")
+)
+
+const (
+	address = "localhost:9558"
+)
+
+func testFlagVersion(t *testing.T) {
+	if _, err := os.Stat(binaryPath); err != nil {
+		t.Skipf("binary not available, try to run `go build` first: %s", err)
+	}
+	exporter := exec.Command(binaryPath, "--version", address)
+	if err := exporter.Run(); err != nil {
+		t.Error(err)
+	}
+}
+
+func testMetricsReturns200(t *testing.T) {
+	if _, err := os.Stat(binaryPath); err != nil {
+		t.Skipf("binary not available, try to run `go build` first: %s", err)
+	}
+
+	exporter := exec.Command(binaryPath, "--web.listen-address", address)
+	test := func(_ int) error {
+		resp, err := queryExporterMetrics()
+		if err != nil {
+			return err
+		}
+		if want, have := http.StatusOK, resp.StatusCode; want != have {
+			return fmt.Errorf("wanted status code %d, received %d", want, have)
+		}
+
+		return nil
+	}
+
+	if err := runCommandAndTests(exporter, address, test); err != nil {
+		t.Error(err)
+	}
+}
+
+func queryExporterMetrics() (*http.Response, error) {
+	return queryExporter(fmt.Sprintf("http://%s/metrics", address))
+}
+
+func queryExporter(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	// b, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// if err := resp.Body.Close(); err != nil {
+	// 	return nil, err
+	// }
+	// if want, have := http.StatusOK, resp.StatusCode; want != have {
+	// 	return nil, fmt.Errorf("want /metrics status code %d, have %d. Body:\n%s", want, have, b)
+	// }
+	return resp, nil
+}
+
+func runCommandAndTests(cmd *exec.Cmd, address string, fn func(pid int) error) error {
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("failed to start command: %s", err)
+	}
+
+	// ensure server is online before running test
+	time.Sleep(10 * time.Millisecond)
+	for i := 0; i < 10; i++ {
+		root := fmt.Sprintf("http://%s/", address)
+		if resp, err := queryExporter(root); err == nil && resp.StatusCode == http.StatusOK {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+		if cmd.Process == nil || i == 9 {
+			return fmt.Errorf("can't connect to %s - unable to run any tests", root)
+		}
+	}
+
+	errc := make(chan error)
+	go func(pid int) {
+		errc <- fn(pid)
+	}(cmd.Process.Pid)
+
+	err := <-errc
+	if cmd.Process != nil {
+		cmd.Process.Kill()
+	}
+	return err
+}
